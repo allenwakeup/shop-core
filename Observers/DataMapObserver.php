a5a3d6d9cf7dbaf62a2b4ffb4a2a131b86ef16b3
Allen Li <314707562@qq.com>
2021-01-29 11:29:12 +0800

    fix module service

--- Observers/DataMapObserver.php
+++ Observers/DataMapObserver.php
@@ -24,10 +24,10 @@ class DataMapObserver
     public function created (DataMap $item)
     {
         // load menu from other module, e.g Lightcms
-        module_tap (self::PERMISSION_SERVICE_ALIAS, function ($service) use ($item) {
+        module_tap (self::PERMISSION_SERVICE_ALIAS, function ($permission_service) use ($item) {
 
             // get menu query builder from application abstract
-            $menu = $service->query ()
+            $menu = $permission_service->query ()
                 ->where ('route', 'admin::' . module_route_prefix ('.') . '.core.dataMap.assignment')
                 ->where ('route_params', '')
                 ->first ()
@@ -36,32 +36,28 @@ class DataMapObserver
             if (isset ($menu))
             {
                 // load root menu from other module
-                module_tap (self::PERMISSION_SERVICE_ALIAS, function ($service) use ($item, $menu) {
-                    $root_menu = $service->retrieve (['route' => $menu->route]);
 
-                    $menu_route = $menu->route . '.' . ($item->left_table . '_' . $item->right_table);
+                $root_menu = $permission_service->retrieve (['route' => $menu->route]);
 
-                    $data = [
-                        'name' => $item->title,
-                        'pid' => empty ($root_menu) ? $menu->id : Arr::get ($root_menu, 'id', $menu->id),
-                        'status' => 1,
-                        'order' => $item->id,
-                        'route' => $menu_route,
-                        'url' => route ('admin::' . module_route_prefix ('.') . 'core.dataMap.assignment', ['id' => $item->id], false) . '?pop=0',
-                        'group' => (isset ($item->data_route) && ! empty ($item->data_route->alias)) ? $item->data_route->alias : $menu->group,
-                        'guard_name' => $menu->guard_name,
-                        'is_lock_name' => 0
-                    ];
+                $menu_route = $menu->route . '.' . ($item->left_table . '_' . $item->right_table);
 
-                    module_tap (self::PERMISSION_SERVICE_ALIAS, function ($service) use ($data, $menu_route) {
-                        $new_menu = $service->save ($data, ['route' => $menu_route]);
-                        if (isset ($new_menu))
-                        {
-                            $this->dispatchChanges ();
-                        }
-                    });
+                $data = [
+                    'name' => $item->title,
+                    'pid' => empty ($root_menu) ? $menu->id : Arr::get ($root_menu, 'id', $menu->id),
+                    'status' => 1,
+                    'order' => $item->id,
+                    'route' => $menu_route,
+                    'url' => route ('admin::' . module_route_prefix ('.') . 'core.dataMap.assignment', ['id' => $item->id], false) . '?pop=0',
+                    'group' => (isset ($item->data_route) && ! empty ($item->data_route->alias)) ? $item->data_route->alias : $menu->group,
+                    'guard_name' => $menu->guard_name,
+                    'is_lock_name' => 0
+                ];
 
-                });
+                $new_menu = $permission_service->save ($data, ['route' => $menu_route]);
+                if (isset ($new_menu))
+                {
+                    $permission_service->flush ();
+                }
 
             }
         });
@@ -72,20 +68,14 @@ class DataMapObserver
     {
         $menu_route = 'admin::' . module_route_prefix ('.') . 'core.dataMap.assignment.' . ($item->left_table . '_' . $item->right_table);
 
-        module_tap ('LightcmsMenuQuery', function ($query) use ($menu_route) {
+        module_tap (self::PERMISSION_SERVICE_ALIAS, function ($permission_service) use ($menu_route) {
 
-            $query->where ('route', $menu_route)->delete ();
+            $permission_service->query ()->where ('route', $menu_route)->delete ();
 
-            $this->dispatchChanges ();
+            $permission_service->flush ();
 
         });
 
-    }
-
-    private function dispatchChanges () {
-        module_tap ('LightcmsFlushMenuJob', function ($job) {
-            dispatch ($job);
-        });
     }
 
 }
