fc2d234dead15beb30e5468ad5f2c8dc16f8288a
Allen Li <ali@goodcatch.cn>
2021-06-11 15:36:06 +0800

    re-construct project

--- src/Repositories/Admin/BaseRepository.php
+++ src/Repositories/Admin/BaseRepository.php
@@ -5,9 +5,8 @@
 
 namespace Goodcatch\Modules\Core\Repositories\Admin;
 
-use Goodcatch\Modules\LightCMS\Repositories\BaseRepository as Repository;
 
-class BaseRepository extends Repository
+class BaseRepository
 {
 
 
